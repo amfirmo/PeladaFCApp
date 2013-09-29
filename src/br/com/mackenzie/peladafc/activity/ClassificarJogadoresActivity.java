56ac81995ae36fb2363281e8dae2fa3e343a3af6
amfirmo <Andre@amf-mapf>
2013-09-29 11:02:30 -0300

    inclusao da atividade de classificacao

---
+++ src/br/com/mackenzie/peladafc/activity/ClassificarJogadoresActivity.java
@@ -0,0 +1,93 @@
+package br.com.mackenzie.peladafc.activity;
+
+import java.util.List;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import br.com.mackenzie.peladafc.adapter.TimesListClassificationAdapter;
+import br.com.mackenzie.peladafc.exception.DAOException;
+import br.com.mackenzie.peladafc.model.Jogador;
+import br.com.mackenzie.peladafc.model.Time;
+
+public class ClassificarJogadoresActivity extends PeladaFCActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_classificar_jogadores);
+		// Show the Up button in the action bar.
+		setupActionBar();
+		
+	    ExpandableListView listView = (ExpandableListView) findViewById(R.id.expandableListView1);
+	    listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+	    TimesListClassificationAdapter adapter = new TimesListClassificationAdapter(this,getTimesFormados());
+	    listView.setAdapter(adapter);
+	}
+	
+	public void classificar(View view){
+		List<Time> times = getTimesFormados();
+		
+		for (Time time : times) {
+			List<Jogador> jogadores = time.getEscalacao();
+			for (Jogador jogador : jogadores) {
+				try {
+					
+					getFacadeController().atualizarJogador(jogador);
+					
+				} catch (DAOException e) {
+					e.printStackTrace();
+					showMessageShort("Não foi possível atualizar o jogador");
+					this.finish();
+				}
+				//showMessageShort(jogador.getNome()+" -> NOTA: "+jogador.getClassificao());
+				
+			}
+		}
+		showMessageShort("Classificações gravadas com sucesso!");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.classificar_jogadores, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ src/br/com/mackenzie/peladafc/adapter/TimesListClassificationAdapter.java
@@ -0,0 +1,130 @@
+package br.com.mackenzie.peladafc.adapter;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+import br.com.mackenzie.peladafc.activity.PeladaFCActivity;
+import br.com.mackenzie.peladafc.activity.R;
+import br.com.mackenzie.peladafc.model.Jogador;
+import br.com.mackenzie.peladafc.model.Time;
+
+public class TimesListClassificationAdapter extends BaseExpandableListAdapter {
+
+  private final List<Time> groups;
+  public LayoutInflater inflater;
+  public Activity activity;
+
+  public TimesListClassificationAdapter(Activity act, List<Time> groups) {
+    activity = act;
+    this.groups = groups;
+    inflater = act.getLayoutInflater();
+  }
+
+  @Override
+  public Object getChild(int groupPosition, int childPosition) {
+    return groups.get(groupPosition).getEscalacao().get(childPosition);
+  }
+
+  @Override
+  public long getChildId(int groupPosition, int childPosition) {
+    return 0;
+  }
+
+  @Override
+  public View getChildView(int groupPosition, final int childPosition,
+      boolean isLastChild, View convertView, ViewGroup parent) {
+    final Jogador children = (Jogador) getChild(groupPosition, childPosition);
+    TextView text = null;
+    RatingBar rat = null;
+    if (convertView == null) {
+      convertView = inflater.inflate(R.layout.listrow_details_classification, null);
+    }
+    text = (TextView) convertView.findViewById(R.id.textView1);
+    text.setText(children.getNome());
+    
+    convertView.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(activity, children.toString(),
+            Toast.LENGTH_SHORT).show();
+      }
+    });
+    
+    rat = (RatingBar) convertView.findViewById(R.id.ratingBar1);
+    rat.setRating(children.getClassificao());
+    
+    rat.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+      @Override
+      public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser){ 
+    	  
+        Toast.makeText(activity, children.getNome()+": NOTA: "+rating, Toast.LENGTH_SHORT).show();
+        //TODO change classificacao to float
+        children.setClassificao((int) rating);
+      }
+    });
+    
+    return convertView;
+  }
+
+  @Override
+  public int getChildrenCount(int groupPosition) {
+    return groups.get(groupPosition).getEscalacao().size();
+  }
+
+  @Override
+  public Object getGroup(int groupPosition) {
+    return groups.get(groupPosition);
+  }
+
+  @Override
+  public int getGroupCount() {
+    return groups.size();
+  }
+
+  @Override
+  public void onGroupCollapsed(int groupPosition) {
+    super.onGroupCollapsed(groupPosition);
+  }
+
+  @Override
+  public void onGroupExpanded(int groupPosition) {
+    super.onGroupExpanded(groupPosition);
+  }
+
+  @Override
+  public long getGroupId(int groupPosition) {
+    return 0;
+  }
+
+  @Override
+  public View getGroupView(int groupPosition, boolean isExpanded,
+      View convertView, ViewGroup parent) {
+    if (convertView == null) {
+      convertView = inflater.inflate(R.layout.listrow_group, null);
+    }
+    Time group = (Time) getGroup(groupPosition);
+    ((CheckedTextView) convertView).setText(group.toString());
+    ((CheckedTextView) convertView).setChecked(isExpanded);
+    return convertView;
+  }
+
+  @Override
+  public boolean hasStableIds() {
+    return false;
+  }
+
+  @Override
+  public boolean isChildSelectable(int groupPosition, int childPosition) {
+    return false;
+  }
+} 
